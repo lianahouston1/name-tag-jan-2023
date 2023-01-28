7391607f47a06e2ad3055e9102b3f8c9b928b90f
Liana Houston <liana.houston1@gmail.com>
2023-01-28 15:49:25 -0800

    variables and functions examples

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 // grab DOM elements
 
 // set event listeners
+console.log('hello universe!');
+console.log(42);
+console.log(42 + 42);
+
+const catName = 'Cinder';
+console.log("My cat's name is Nero.");
+console.log(`My cat's name is ${catName}.`);
+
+function sayHello(name) {
+    console.log(`Hello ${name}!`);
+    console.log('It is a drizzle day today.');
+    console.log('It is v sunny.');
+}
+
+sayHello('Ryan');
+
